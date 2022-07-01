a4c3ce09ddacc5f16e89bb53e44cc6b6d6fd301c
xCodetic <108384168+xCodetic@users.noreply.github.com>
2022-07-01 14:52:11 +0000

    Deploying to gh-pages from @ xCodetic/awesometarot@27f9f79465facf527b4ed8df02b17ed7f8b9acfe 🚀

---
+++ _next/static/yntx1Cluc-fdvObm35NMn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/adf6895ec356eb64.css","static/chunks/pages/index-9a47f85bd2a72687.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
