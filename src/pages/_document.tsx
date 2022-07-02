8c3949cf739768f2fe1d039dc0c0fa1e9488dbf8
Hoang Nguyen <h.nguyen@schhanoi.com>
2022-07-02 12:26:44 +0200

    use _document instead of custome header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,87 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
+import { generateUrl } from 'utils'
+
+const THEME_COLOR = process.env.NEXT_PUBLIC_THEME_COLOR;
+const MANIEFST_PATH = process.env.NEXT_PUBLIC_MANIEFST_PATH;
+const ICON_192_PATH = process.env.NEXT_PUBLIC_ICON_192_PATH;
+const FAV_ICON_PATH = process.env.NEXT_PUBLIC_FAV_ICON_PATH;
+
 class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <link rel="manifest" href="/awesometarot/manifest.json" />
-                    <link rel="apple-touch-icon" href="/awesometarot/icon.png"></link>
-                    <meta name="theme-color" content="#fff" />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+	render() {
+		const description = '',
+			charset = 'utf-8',
+			hrefManifest = MANIEFST_PATH,
+			themeColor = THEME_COLOR,
+			favIconPath = FAV_ICON_PATH,
+			keywords = '',
+			appleIconPath = ICON_192_PATH,
+			appleIconSize = '192x192';
+
+		return (
+			<Html>
+				<Head>
+					<meta charSet={charset} key="charSet" />
+					<meta name="description" key="description" content={description} />
+					{themeColor && (
+						<meta name="theme-color" key="theme-color" content={themeColor} />
+					)}
+					<meta name="keywords" key="keywords" content={keywords} />
+					<meta httpEquiv="X-UA-Compatible" key="ua-compatible" content="ie=edge" />
+					{hrefManifest && (
+						<link rel="manifest" href={generateUrl(hrefManifest)} />
+					)}
+					{favIconPath && (
+						<link rel="shortcut icon" href={generateUrl(favIconPath)} />
+					)}
+
+					{/* for safari */}
+					<meta
+						name="apple-mobile-web-app-capable"
+						key="apple-mobile-web-app-capable"
+						content="yes"
+					/>
+					<meta
+						name="apple-mobile-web-app-status-bar-style"
+						key="apple-mobile-web-app-status-bar-style"
+						content="blue"
+					/>
+					<meta
+						name="apple-mobile-web-app-title"
+						key="apple-mobile-web-app-title"
+						content="With Manifest"
+					/>
+					{appleIconPath && appleIconSize && (
+						<link
+							rel="apple-touch-icon"
+							sizes={appleIconSize}
+							href={generateUrl(appleIconPath)}
+						/>
+					)}
+
+					{/* for IE */}
+					{appleIconPath && (
+						<meta
+							name="msapplication-TitleImage"
+							key="msapplication-TitleImage"
+							content={generateUrl(appleIconPath)}
+						/>
+					)}
+					{themeColor && (
+						<meta
+							name="msapplication-TitleColor"
+							key="msapplication-TitleColor"
+							content={themeColor}
+						/>
+					)}
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
 export default MyDocument;
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
 import { join } from 'path';
 
+const LINK_PREFIX = process.env.NEXT_PUBLIC_LINK_PREFIX || '';
+
 export const generateUrl = (itemPath: string) => {
-    return join('/awesometarot', itemPath);
+    return join(LINK_PREFIX, itemPath);
 }
